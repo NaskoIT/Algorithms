6239d4e745ce1aa0111955cdc42b2771ee7bfdb0
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-12-13 20:49:53 +0200

    Tower of Hanoi solution.

--- RECURSION/Recursion-Exercise/TowerOfHanoi/Program.cs
+++ RECURSION/Recursion-Exercise/TowerOfHanoi/Program.cs
@@ -7,46 +7,46 @@ namespace TowerOfHanoi
     class Program
     {
         private static int stepsTaken = 0;
+        private static Stack<int> source;
+        private static readonly Stack<int> destination = new Stack<int>();
+        private static readonly Stack<int> spare = new Stack<int>();
 
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] disks = Enumerable.Range(1, n).ToArray();
-            Stack<int> source = new Stack<int>(disks.Reverse());
-            Stack<int> destination = new Stack<int>();
-            Stack<int> spare = new Stack<int>();
+            int numberOfDisks = int.Parse(Console.ReadLine());
+            IEnumerable<int> disks = Enumerable.Range(1, numberOfDisks);
+            source = new Stack<int>(disks.Reverse());
 
-            PrintRods(source, destination, spare);
-            Solve(source, destination, spare, n);
+            PrintRods();
+            Solve(source, destination, spare, numberOfDisks);
         }
 
         private static void Solve(Stack<int> source, Stack<int> destination, Stack<int> spare, int bottomDisk)
         {
             if (bottomDisk == 1)
             {
-                destination.Push(source.Pop());
                 stepsTaken++;
-                Console.WriteLine($"Step #{stepsTaken}: Moved disk {bottomDisk}");
-                PrintRods(source, destination, spare);
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{stepsTaken}: Moved disk");
+                PrintRods();
                 return;
             }
             else
             {
-                stepsTaken++;
-                Console.WriteLine($"Step #{stepsTaken}: Moved disk {bottomDisk}");
-                PrintRods(source, destination, spare);
-
+                //Move disks on top of bottomDisk from source to spare
                 Solve(source, spare, destination, bottomDisk - 1);
-                destination.Push(source.Pop());
-                Solve(spare, destination, source, bottomDisk - 1);
 
                 stepsTaken++;
-                Console.WriteLine($"Step #{stepsTaken}: Moved disk {bottomDisk}");
-                PrintRods(source, destination, spare);
+                destination.Push(source.Pop());
+                Console.WriteLine($"Step #{stepsTaken}: Moved disk");
+                PrintRods();
+
+                //MOve disks previously moved to spare to destination
+                Solve(spare, destination, source, bottomDisk - 1);
             }
         }
 
-        private static void PrintRods(Stack<int> source, Stack<int> destination, Stack<int> spare)
+        private static void PrintRods()
         {
             Console.WriteLine($"Source: {string.Join(", ", source.Reverse())}");
             Console.WriteLine($"Destination: {string.Join(", ", destination.Reverse())}");
