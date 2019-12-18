5173368d4b8392d5b43434bbeb9922fb4fe3306c
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-12-18 20:55:28 +0200

    Solve BestLecturesSchedule problem and classical EgyptianFractions problem

---
+++ GREEDY ALGORITHMS/GreedyAlgorithms-Exercise/BestLecturesSchedule/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestLecturesSchedule
+{
+    class Program
+    {
+        private static readonly List<Lecture> lectures = new List<Lecture>();
+
+        static void Main(string[] args)
+        {
+            PraseInput();
+
+            var schedule = FindBestShedule();
+
+            PrintSchedule(schedule);
+        }
+
+        private static void PrintSchedule(List<Lecture> schedule)
+        {
+            Console.WriteLine($"Lectures ({schedule.Count}):");
+
+            foreach (var lecture in schedule)
+            {
+                Console.WriteLine($"{lecture.StartTime}-{lecture.EndTime} -> {lecture.Name}");
+            }
+        }
+
+        private static List<Lecture> FindBestShedule()
+        {
+            var result = new List<Lecture>();
+            var sortedLectures = new HashSet<Lecture>(lectures.OrderBy(x => x.EndTime));
+            
+            while(sortedLectures.Any())
+            {
+                Lecture currentLecture = sortedLectures.First();
+                result.Add(currentLecture);
+
+                foreach (var lectureToRemove in sortedLectures.Where(x => x.StartTime < currentLecture.EndTime).ToList())
+                {
+                    sortedLectures.Remove(lectureToRemove);
+                }
+            }
+
+            return result;
+        }
+
+        private static void PraseInput()
+        {
+            int lecturesCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+
+            for (int i = 0; i < lecturesCount; i++)
+            {
+                string[] lectureParts = Console.ReadLine().Split(new char[] { ':', ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                string lectureName = lectureParts[0];
+                int startTime = int.Parse(lectureParts[1]);
+                int endTime = int.Parse(lectureParts[2]);
+
+                lectures.Add(new Lecture
+                {
+                    EndTime = endTime,
+                    Name = lectureName,
+                    StartTime = startTime
+                });
+            }
+        }
+    }
+
+    public class Lecture
+    {
+        public string Name { get; set; }
+
+        public int StartTime { get; set; }
+
+        public int EndTime { get; set; }
+    }
+}
---
+++ GREEDY ALGORITHMS/GreedyAlgorithms-Exercise/EgyptianFractions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EgyptianFractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberParts = Console.ReadLine().Split('/').Select(int.Parse).ToArray();
+            int numerator = numberParts[0];
+            int denominator = numberParts[1];
+
+            if(numerator > denominator)
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+
+            List<int> denominators = GetFrations(numerator, denominator);
+
+            Console.Write($"{numerator}/{denominator} = ");
+            Console.WriteLine(string.Join(" + ", denominators.Select(x => $"1/{x}")));
+        }
+
+        private static List<int> GetFrations(int numerator, int denominator)
+        {
+            var denominators = new List<int>();
+
+            int nextDenominator = 2;
+            while(numerator > 0)
+            {
+                int newNumerator = numerator * nextDenominator;
+                int nextNumerator = denominator;
+
+                if(newNumerator >= nextNumerator)
+                {
+                    newNumerator -= nextNumerator;
+                    numerator = newNumerator;
+                    denominator *= nextDenominator;
+                    denominators.Add(nextDenominator);
+                }
+
+                nextDenominator++;
+            }
+
+            return denominators;
+        }
+    }
+}
