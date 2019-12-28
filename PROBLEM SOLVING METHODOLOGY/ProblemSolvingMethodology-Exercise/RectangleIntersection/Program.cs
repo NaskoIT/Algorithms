1091062826007006929d3c1458f0afd5a841a44a
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-12-28 19:06:52 +0200

    ProblemSolvingMethodology-Exercise - ShortestPathInMatrix, RectangleIntersection

---
+++ PROBLEM SOLVING METHODOLOGY/ProblemSolvingMethodology-Exercise/RectangleIntersection/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace RectangleIntersection
+{
+    class Program
+    {
+        private static Rectangle[] rectangles;
+
+        static void Main(string[] args)
+        {
+            int rectanglesCount = int.Parse(Console.ReadLine());
+            rectangles = new Rectangle[rectanglesCount];
+            int[] xCoordinates = new int[rectangles.Length * 2];
+            int[] yCoordinates = new int[rectangles.Length * 2];
+            int index = 0;
+
+            for (int i = 0; i < rectanglesCount; i++)
+            {
+                int[] rectangleCoordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                rectangles[i] = new Rectangle
+                {
+                    MinX = rectangleCoordinates[0],
+                    MaxX = rectangleCoordinates[1],
+                    MinY = rectangleCoordinates[2],
+                    MaxY = rectangleCoordinates[3]
+                };
+
+                xCoordinates[index] = rectangleCoordinates[0];
+                yCoordinates[index] = rectangleCoordinates[2];
+                index++;
+                xCoordinates[index] = rectangleCoordinates[1];
+                yCoordinates[index] = rectangleCoordinates[3];
+                index++;
+            }
+
+            int area = GetRectanglesIntersectionArea(xCoordinates, yCoordinates);
+            Console.WriteLine(area);
+        }
+
+        private static int GetRectanglesIntersectionArea(int[] xCoordinates, int[] yCoordinates)
+        {
+            Array.Sort(xCoordinates);
+            Array.Sort(yCoordinates);
+            int totalArea = 0;
+
+            for (int i = 0; i < xCoordinates.Length - 1; i++)
+            {
+                int currentX = xCoordinates[i];
+                int nextX = xCoordinates[i + 1];
+
+                totalArea += GetInervalIntersectionArea(yCoordinates, currentX, nextX);
+            }
+
+            return totalArea;
+        }
+
+        private static int GetInervalIntersectionArea(int[] yCoordinates, int currentX, int nextX)
+        {
+            int intervalArea = 0;
+            int xIntervalRange = nextX - currentX;
+            var overlappingRectngles = rectangles.Where(rectangle => rectangle.InXInterval(currentX, nextX));
+            
+            if(overlappingRectngles.Count() < 2)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < yCoordinates.Length - 1; i++)
+            {
+                int currentY = yCoordinates[i];
+                int nextY = yCoordinates[i + 1];
+                int yIntervalRange = nextY - currentY;
+
+                int overlappingRectanglesCount = overlappingRectngles.Count(rectangle => rectangle.InYInterval(currentY, nextY));
+
+                if(overlappingRectanglesCount > 1)
+                {
+                    intervalArea += xIntervalRange * yIntervalRange;
+                }
+            }
+
+            return intervalArea;
+        }
+    }
+    
+    public class Rectangle
+    {
+        public int MinX { get; set; }
+
+        public int MaxX { get; set; }
+
+        public int MinY { get; set; }
+
+        public int MaxY { get; set; }
+
+        public bool InXInterval(int intervalStart, int inervalEnd)
+        {
+            return MinX < inervalEnd && MaxX > intervalStart;
+        }
+
+        public bool InYInterval(int intervalStart, int inervalEnd)
+        {
+            return MinY < inervalEnd && MaxY > intervalStart;
+        }
+    }
+}
---
+++ PROBLEM SOLVING METHODOLOGY/ProblemSolvingMethodology-Exercise/ShortestPathInMatrix/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortestPathInMatrix
+{
+    class Program
+    {
+        private static int rows;
+        private static int cols;
+        private static int[][] matrix;
+        static void Main(string[] args)
+        {
+            rows = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            matrix = new int[rows][];
+
+            FillMatrix();
+
+            IEnumerable<int> path = FindPath(0, 0, rows - 1, cols - 1);
+            Console.WriteLine($"Length: {path.Sum()}");
+            Console.WriteLine($"Path: {string.Join(" ", path)}");
+        }
+
+        private static IEnumerable<int> FindPath(int sourceRow, int sourceCol, int destinationRow, int destinationCol)
+        {
+            Cell[,] previous = new Cell[rows, cols];
+            bool[,] used = new bool[rows, cols];
+            int[,] distances = new int[rows, cols];
+            FillDistancesWithDefaultValues(distances);
+
+            distances[sourceRow, sourceCol] = 0;
+            Cell[] neighbourCells = GetNeighbourCells();
+
+            while (true)
+            {
+                int minDistance = int.MaxValue;
+                Cell currentCell = null;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (!used[row, col] && distances[row, col] < minDistance)
+                        {
+                            minDistance = distances[row, col];
+                            currentCell = new Cell(row, col);
+                        }
+
+                    }
+                }
+
+                used[currentCell.Row, currentCell.Col] = true;
+
+                foreach (var neighbourCell in neighbourCells)
+                {
+                    int row = currentCell.Row + neighbourCell.Row;
+                    int col = currentCell.Col + neighbourCell.Col;
+
+                    if (InBounds(row, col))
+                    {
+                        int newDistance = distances[currentCell.Row, currentCell.Col] + matrix[row][col];
+                        if (newDistance < distances[row, col])
+                        {
+                            distances[row, col] = newDistance;
+                            previous[row, col] = currentCell;
+                        }
+                    }
+                }
+
+                if(currentCell.Row == destinationRow && currentCell.Col == destinationCol)
+                {
+                    break;
+                }
+            }
+
+            var path = new Stack<int>();
+            Cell pathCell = new Cell(destinationRow, destinationCol);
+
+            while (pathCell != null)
+            {
+                path.Push(matrix[pathCell.Row][pathCell.Col]);
+                pathCell = previous[pathCell.Row, pathCell.Col];
+            }
+
+            return path;
+        }
+
+        private static void FillDistancesWithDefaultValues(int[,] distances)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    distances[row, col] = int.MaxValue;
+                }
+            }
+        }
+
+        private static Cell[] GetNeighbourCells()
+        {
+            return new Cell[]
+            {
+                new Cell(0, 1),
+                new Cell(1, 0),
+                new Cell(-1, 0),
+                new Cell(0, -1)
+            };
+        }
+
+        private static bool InBounds(int row, int col)
+        {
+            return row >= 0 && row < rows && col >= 0 && col < cols;
+        }
+
+        private static void FillMatrix()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+        }
+
+        private class Cell
+        {
+            public Cell(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+
+            public int Row { get; set; }
+
+            public int Col { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                Cell otherNode = (Cell)obj;
+                return otherNode.Row == Row && otherNode.Col == Col;
+            }
+
+            public override int GetHashCode()
+            {
+                return Row * 7 ^ Col;
+            }
+        }
+    }
+}
