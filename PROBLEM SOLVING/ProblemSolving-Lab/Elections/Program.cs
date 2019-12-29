a1adc5a6a96499a33374211ac025b57a2143ab12
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-12-29 15:05:08 +0200

    Problem solving resources. Elections, SumTo13 and Lumber tasks solutions

---
+++ PROBLEM SOLVING/ProblemSolving-Lab/Elections/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+
+namespace Elections
+{
+    class Program
+    {
+        private static int[] parties;
+
+        static void Main(string[] args)
+        {
+            int neededVotes = int.Parse(Console.ReadLine());
+            int partiesCount = int.Parse(Console.ReadLine());
+
+            parties = new int[partiesCount];
+            int maxVotes = 0;
+
+            for (int i = 0; i < partiesCount; i++)
+            {
+                parties[i] = int.Parse(Console.ReadLine());
+                maxVotes += parties[i];
+            }
+
+            Array.Sort(parties);
+            BigInteger[] votes = new BigInteger[maxVotes + 1];
+            votes[0] = 1;
+            int mostSeats = 0;
+
+            foreach (var partyVotes in parties)
+            {
+                for (int currentSeats = mostSeats + partyVotes; currentSeats >= partyVotes; currentSeats--)
+                {
+                    if (votes[currentSeats - partyVotes].IsZero)
+                    {
+                        continue;
+                    }
+
+                    if (mostSeats < currentSeats)
+                    {
+                        mostSeats = currentSeats;
+                    }
+
+                    votes[currentSeats] += votes[currentSeats - partyVotes];
+                }
+            }
+
+            BigInteger combinations = 0;
+
+            for (int i = neededVotes; i <= maxVotes; i++)
+            {
+                if (votes[i] == 0)
+                {
+                    continue;
+                }
+
+                combinations += votes[i];
+            }
+
+            Console.WriteLine(combinations);
+        }
+    }
+}
---
+++ PROBLEM SOLVING/ProblemSolving-Lab/SumTo13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SumTo13
+{
+    class Program
+    {
+        private const string Yes = "Yes";
+        private const string No = "No";
+        private const int TargetSum = 13;
+
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string result = Sum(numbers, 0, 0) ? Yes : No;
+            Console.WriteLine(result);
+        }
+
+        private static bool Sum(int[] numbers, int index, int currentSum)
+        {
+            if(index >= numbers.Length)
+            {
+                return TargetSum == currentSum;
+            }
+
+            return Sum(numbers, index + 1, currentSum + numbers[index]) || 
+                   Sum(numbers, index + 1, currentSum - numbers[index]);
+        }
+    }
+}
